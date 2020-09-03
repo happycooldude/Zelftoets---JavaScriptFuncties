0252675027b27c7a9cfb187981d26f46949baaa0
Pieter <pieterhuisman2000@gmail.com>
2020-09-03 14:16:09 +0200

    comments neergezet bij de verschillende functies wat ze moeten doen en of ze al werken.

--- Duckhunt/script.js
+++ Duckhunt/script.js
@@ -1,39 +1,41 @@
-var points = 0;
+var points = 1;
 var misses = 0;
 
+//maak de achtergrond aan en zorg dat de strafpunten bij elkaar opgeteld worden (de achtergrond werkt|het klikken en de optelling werkt nog niet)
 document.body.style.backgroundImage = "url('stage.png')";
 document.body.onclick = function miss(){
     misses + 1;
     console.log(misses);
 }
 
+//maak de duck aan en zet hem in het midden van het scherm (half werkend)
 function createDuck(){
     var duck = document.createElement("DIV");
     duck.id = 'duck';
     duck.style.width = "250px";
     duck.style.height = "250px";
     duck.style.position = "relative"
-    duck.style.top = 200;
-    duck.style.left = 830;
+    var top = duck.style.top = 200;
+    var left = duck.style.left = 830;
     duck.style.backgroundImage = "url('duck.gif')";
     document.body.appendChild(duck);
 }
 
+//Hier moeten er punten worden opgeteld als je op de DIV van de duck klikt (nog niet werkend)
 // document.getElementById("duck").onclick = function hit(){
 //     points +1;
 //     console.log(points);
 // }
 
+//de array met de verschillende richtingen en de timer die in de timer functie gebruikt wordt (werkend)
 var direction = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']; 
 
 var interval = setInterval(timer, 1000);
 
 createDuck();
 
+//laat om de 1,5 seconden een random richting kiezen uit de array boven. en zorgt dat de duck kan bewegen (random richting kiezen werkt | de duck laten bewegen werkt nog niet)
 function timer() {
-    
-    var top = 200;
-    var left = 830;
         var random = Math.floor(Math.random() * direction.length);
         console.log(random, direction[random]);
         console.log(top,left);
