32d1364ad9ca617f6bf4b075f0449e3afbcde9d0
Pieter <pieterhuisman2000@gmail.com>
2020-09-03 14:02:42 +0200

    probeer onclick funtie te maken als je op de achtergrond klikt en op de duck

--- Duckhunt/script.js
+++ Duckhunt/script.js
@@ -1,65 +1,82 @@
+var points = 0;
+var misses = 0;
+
 document.body.style.backgroundImage = "url('stage.png')";
+document.body.onclick = function miss(){
+    misses + 1;
+    console.log(misses);
+}
 
 function createDuck(){
     var duck = document.createElement("DIV");
+    duck.id = 'duck';
     duck.style.width = "250px";
     duck.style.height = "250px";
     duck.style.position = "relative"
-    duck.style.top = "200";
-    duck.style.left = "830";
+    duck.style.top = 200;
+    duck.style.left = 830;
     duck.style.backgroundImage = "url('duck.gif')";
     document.body.appendChild(duck);
 }
-createDuck();
+
+// document.getElementById("duck").onclick = function hit(){
+//     points +1;
+//     console.log(points);
+// }
 
 var direction = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']; 
 
-var interval = setInterval(timer, 1500);
+var interval = setInterval(timer, 1000);
+
+createDuck();
 
 function timer() {
+    
+    var top = 200;
+    var left = 830;
         var random = Math.floor(Math.random() * direction.length);
         console.log(random, direction[random]);
         console.log(top,left);
     switch(direction){
-        case "N":
-            duck.style.top + 10;
-            duck.style.top = top + "px";
+        case 'N':
+            top + 10;
+            duck.style.marginTop = top + "px";
         break
-        case "NE":
-            duck.style.top + 10;
-            duck.style.left + 10;
-            duck.style.top = top + "px";
-            duck.style.left = left + "px";
+        case 'NE':
+            top + 10;
+            left + 10;
+            duck.style.marginTop = top + "px";
+            duck.style.marginLeft = left + "px";
         break
-        case "E":
-            duck.style.left + 10;
-            duck.style.left = left + "px";
+        case 'E':
+            left + 10;
+            duck.style.marginLeft = left + "px";
         break
-        case "SE":
-            duck.style.top - 10;
-            duck.style.left + 10;
-            duck.style.top = top + "px";
-            duck.style.left = left + "px";
+        case 'SE':
+            top - 10;
+            left + 10;
+            duck.style.marginTop = top + "px";
+            duck.style.marginLeft = left + "px";
         break
-        case "S":
-            duck.style.top - 10;
-            duck.style.left = top + "px";
+        case 'S':
+            top - 10;
+            duck.style.marginTop = top + "px";
         break
-        case "SW":
-            duck.style.top - 10;
-            duck.style.left - 10;
-            duck.style.top = top + "px";
-            duck.style.left = left + "px";
+        case 'SW':
+            top - 10;
+            left - 10;
+            duck.style.marginTop = top + "px";
+            duck.style.marginLeft = left + "px";
         break
-        case "W":
-            duck.style.left - 10;
-            duck.style.left = left + "px";
+        case 'W':
+            left - 10;
+            duck.style.marginLeft = left + "px";
         break
-        case "NW":
-            duck.style.top + 10;
-            duck.style.left - 10;
-            duck.style.top = top + "px";
-            duck.style.left = left + "px";
+        case 'NW':
+            top + 10;
+            left - 10;
+            duck.style.marginTop = top + "px";
+            duck.style.marginLeft = left + "px";
         break
     }
 }
